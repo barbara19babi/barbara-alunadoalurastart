ecdffc3b2ce8b5daf2bf3987fb0cf8a06949b535
barbara19babi <00001098608288sp@al.educacao.sp.gov.br>
2024-09-23 16:57:14 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+
+  let palavras = ["Problema", "Problem", "Prob"];
+
+  palavra = random(palavras);
+}
+
+function inicializaCores() {lav
+  background("red");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  inicializaCores();
+
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+}
