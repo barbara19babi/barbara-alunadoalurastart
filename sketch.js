5ee45ff2e530b19d89d1a292840d35c1802f6170
barbara19babi <00001098608288sp@al.educacao.sp.gov.br>
2024-09-26 17:54:28 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,30 @@
-
+let angle = 0;
+let gridSize = 5; // Number of cubes in each row and column
+let cubeSize = 40; // Size of each cube
 
 function setup() {
-  createCanvas(400, 400);
-
-  let palavras = ["Problema", "Problem", "Prob"];
-
-  palavra = random(palavras);
-}
-
-function inicializaCores() {lav
-  background("red");
-  fill("black");
-  textSize(64);
-  textAlign(CENTER, CENTER);
+  createCanvas(400, 400, WEBGL);
+  noFill();
 }
 
 function draw() {
-  inicializaCores();
-
-  let maximo = width;
-  let minimo = 0;
-  // mouseX, 0, width ==> 0, palavra.length
-
-  let quantidade = map(mouseX, 0, width, 1, palavra.length);
-  //console.log(quantidade);
-  let parcial = palavra.substring(0, quantidade);
-  text(parcial, 200, 200);
+  background(220);
+  
+  let spacing = cubeSize * 1.5;
+  translate(-spacing * (gridSize - 1) / 2, -spacing * (gridSize - 1) / 2, 0);
+  
+  for (let x = 0; x < gridSize; x++) {
+    for (let y = 0; y < gridSize; y++) {
+      push();
+      translate(x * spacing, y * spacing, 0);
+      rotateX(angle);
+      rotateY(angle * 0.3);
+      box(cubeSize);
+      pop();
+    }
+  }
+  
+  angle += 0.03; // Increase to rotate faster
 }
+
+
